6b3a4f5a5b5f77c09b7d12ef353250b7521813da
stenterello <davide.delladio@gmail.com>
2023-01-04 19:28:16 +0100

    next step

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -10,7 +10,7 @@ done
 wp core download --path=wp_files --allow-root
 wp config create --dbname=$WP_DATABASE_NAME --dbuser=$WP_DATABASE_USR --dbpass=$WP_DATABASE_PWD --dbhost=$DB_HOSTNAME --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
 wp db create --allow-root
-wp core install --url=$DOMAIN_NAME/wordpress --title="Inception" --admin_user=$WP_DATABASE_USR --admin_password=$WP_DATABASE_PWD --admin_email=ddelladi@student.42roma.it --skip-email --allow-root
+wp core install --url=$DOMAIN_NAME/wordpress --title="Inception" --admin_user=$WP_DATABASE_USR --admin_password=$WP_DATABASE_PWD --admin_email=$WP_DATABASE_EMAIL --skip-email --allow-root
 wp user create $WP_DATABASE_USR $WP_DATABASE_EMAIL --role=author --user_pass=$WP_DATABASE_PWD --allow-root
 
 mv /tmp/index.html /var/www/html/index.html
