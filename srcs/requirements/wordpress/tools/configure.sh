9959345a3be8b0af5fb80d1b32aad89463c9c69c
stenterello <davide.delladio@gmail.com>
2023-01-06 15:16:05 +0100

    request something does not mean you can have it

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -7,12 +7,15 @@ do
     sleep 3
 done
 
-wp core download --path=wp_files --allow-root
-wp config create --dbname=$WP_DATABASE_NAME --dbuser=$WP_DATABASE_USR --dbpass=$WP_DATABASE_PWD --dbhost=$DB_HOSTNAME --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
-wp db create --allow-root
-wp core install --url=$DOMAIN_NAME/wordpress --title="Inception" --admin_user=$WP_DATABASE_USR --admin_password=$WP_DATABASE_PWD --admin_email=$WP_DATABASE_EMAIL --skip-email --allow-root
-wp user create $WP_DATABASE_USR $WP_DATABASE_EMAIL --role=author --user_pass=$WP_DATABASE_PWD --allow-root
+if [ ! -f "/var/www/html/wordpress/index.php" ];
+then
+	wp core download --allow-root
+	wp config create --dbname=$WP_DATABASE_NAME --dbuser=$WP_DATABASE_USR --dbpass=$WP_DATABASE_PWD --dbhost=$DB_HOSTNAME --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+	wp db create --allow-root
+	wp core install --url=$DOMAIN_NAME --title="Inception" --admin_user=$WP_DATABASE_USR --admin_password=$WP_DATABASE_PWD --admin_email=$WP_DATABASE_EMAIL --skip-email --allow-root
+	wp theme activate twentytwentythree --allow-root
+fi
 
-mv /tmp/index.html /var/www/html/index.html
-
+echo "OK"
 /usr/sbin/php-fpm7 -F -R
+echo "OK2"
