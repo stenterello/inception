66eebc91e2959bfaf251f668b1c6e157836d12f4
stenterello <davide.delladio@gmail.com>
2023-01-15 14:00:46 +0100

    wordpress working

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -11,9 +11,8 @@ if [ ! -f "/var/www/html/wordpress/index.php" ];
 then
 	wp core download --allow-root
 	wp config create --dbname=$DATABASE_NAME --dbuser=$DATABASE_ADMIN --dbpass=$DATABASE_ADMIN_PWD --dbhost=$DB_HOSTNAME --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
-	wp db create --allow-root
-	wp core install --url=$DOMAIN_NAME --title="Inception" --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-	wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --skip-email --allow-root
+	wp core install --url=$DOMAIN_NAME/wordpress --title="Inception" --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+	wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
 	wp theme activate twentytwentythree --allow-root
 else
 	echo "Wordpress already installed";
